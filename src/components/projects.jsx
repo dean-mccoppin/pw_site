2f064db4a5d334c683e6371b848f4945c81732f0
truetiming <truetiming@gmail.com>
2021-11-03 21:42:36 -0400

    projects almost done, just fix nav transition

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -37,6 +37,52 @@ function Projects() {
             Tweet anonymously without signing in or signing up. Utilizes Redis
             for live communication between all connected clients to the server.
           </span>
+
+          <ul className="proj-labels">
+            <li>React</li>
+            <li>Django REST API</li>
+            <li>Redis</li>
+            <li>PostgreSQL</li>
+            <li>Websockets</li>
+            <li>Google Cloud Platform</li>
+          </ul>
+        </div>
+        <div className="proj">
+          <img src={test} />
+          <div className="proj-titles">
+            <span>Reddit Algorithm</span>
+            <div>
+              <Button
+                size="lg"
+                radius="lg"
+                styles={{
+                  root: {
+                    backgroundColor: "#f5f6fa !important",
+                  },
+                  label: {
+                    fontSize: 18,
+                    color: "#000000 !important",
+                  },
+                }}
+                id="meeting-btn"
+              >
+                View Project
+              </Button>
+            </div>
+          </div>
+          <span className="proj-desc">
+            Determine the political bias of a Reddit thread by analyzing each
+            commentators post history and ranking their partisanship.
+          </span>
+
+          <ul className="proj-labels">
+            <li>React</li>
+            <li>Django</li>
+            <li>Celery</li>
+            <li>Redis</li>
+            <li>Reddit API</li>
+            <li>Heroku</li>
+          </ul>
         </div>
       </div>
     </div>
