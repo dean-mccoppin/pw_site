f09e1a7fc2c9357f0c6f7ca6f82cd92fc7e4f71e
truetiming <truetiming@gmail.com>
2021-11-08 14:29:29 -0500

    final update

--- src/App.js
+++ src/App.js
@@ -13,9 +13,9 @@ function App() {
       <div className="wrapper">
         <NavParent />
         <Routes>
-          <Route exact path="/" component={<Home />} />
-          <Route exact path="/about" component={<About />} />
-          <Route exact path="/projects" component={<Projects />} />
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/about" element={<About />} />
+          <Route exact path="/projects" element={<Projects />} />
         </Routes>
         <Footer />
       </div>
