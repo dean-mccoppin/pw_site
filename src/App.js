f8619627619c6bf463e9283b582dff78ebc20357
truetiming <truetiming@gmail.com>
2021-11-08 14:15:30 -0500

    fixed routers

--- src/App.js
+++ src/App.js
@@ -5,18 +5,18 @@ import About from "./components/about.jsx";
 import Footer from "./components/footer.jsx";
 import Projects from "./components/projects.jsx";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <Router>
       <div className="wrapper">
         <NavParent />
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/about" component={About} />
-          <Route exact path="/projects" component={Projects} />
-        </Switch>
+        <Routes>
+          <Route exact path="/" component={<Home />} />
+          <Route exact path="/about" component={<About />} />
+          <Route exact path="/projects" component={<Projects />} />
+        </Routes>
         <Footer />
       </div>
     </Router>
